83221f8fefc9cce3d18f78734b34162d9279e647
penspanic <penspanic@gmail.com>
2025-07-18 15:34:21 +0900

    Allow property setter, add setter usage Analyzer
     - Need setter for deserialization

--- Datra.Test/Models/RefTestData.cs
+++ Datra.Test/Models/RefTestData.cs
@@ -1,11 +1,11 @@
-using Datra.Data.Attributes;
-using Datra.Data.Interfaces;
-
-namespace Datra.Test.Models
-{
-    [TableData("Characters.csv", Format = DataFormat.Csv)]
-    public partial class RefTestData : ITableData<string>
-    {
-        public string Id { get; }
-    }
-}
+// using Datra.Data.Attributes;
+// using Datra.Data.Interfaces;
+//
+// namespace Datra.Test.Models
+// {
+//     [TableData("Characters.csv", Format = DataFormat.Csv)]
+//     public partial class RefTestData : ITableData<string>
+//     {
+//         public string Id { get; set; }
+//     }
+// }
